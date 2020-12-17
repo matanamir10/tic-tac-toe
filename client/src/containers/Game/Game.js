639f60788f961ff04ff637db2cb5b762bcc3e696
matan11 <matan6080@gmail.com>
2020-12-17 13:38:36 +0200

    Added Board component to Game container

--- client/src/containers/Game/Game.js
+++ client/src/containers/Game/Game.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Game.scss';
+import {Board} from '../../components/Board/Board';
 
 export const Game = () => {
     return (
     <div className='game'>
-            
+     <Board/>       
      </div>
     )
 }
