fc844e96edb3bd5cadaefdd5940b2d991fd8ba0d
matan11 <matan6080@gmail.com>
2020-12-17 13:50:33 +0200

    Game render MatchDetails component

--- client/src/containers/Game/Game.js
+++ client/src/containers/Game/Game.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Game.scss';
 import {Board} from '../../components/Board/Board';
+import { MatchDetails } from '../../components/MatchDetails/MatchDetails';
 
 export const Game = () => {
     return (
     <div className='game'>
+    <MatchDetails/>
      <Board/>       
      </div>
     )
