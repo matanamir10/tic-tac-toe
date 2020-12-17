612d555caed0c945119471adb3c0415eec602fd8
matan11 <matan6080@gmail.com>
2020-12-17 14:09:12 +0200

    chnaged prop name from id to value

--- client/src/components/Square/Square.js
+++ client/src/components/Square/Square.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Square.scss';
 
-export const Square = ({id}) => {
+export const Square = ({value}) => {
     return (
         <div className='square'>
-            <span className='square__text'>{id}</span>   
+            <span className='square__text'>{value}</span>   
         </div>
     )
 }
