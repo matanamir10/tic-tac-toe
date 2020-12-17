e6a2b5b56f256b4c86c515db48644675d8d32945
matan11 <matan6080@gmail.com>
2020-12-17 13:40:37 +0200

    Game Setup

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Game } from '../Game/Game';
 import './App.scss';
 
 export const App = () => {
   return (
     <div className='container'>
+      <Game/>
     </div>
   )
 }
