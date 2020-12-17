b5f1a916519122dedb1f74c893c5301eafbeacb3
matan11 <matan6080@gmail.com>
2020-12-17 16:36:33 +0200

    Addde oponnetProvier to Index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import {App} from './containers/App/App';
+import {OponentProvider} from './context/Oponent';
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <OponentProvider>
     <App />
+    </OponentProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
