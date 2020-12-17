03710a76e00d7c6e2e472e25115c06334498e3d5
matan11 <matan6080@gmail.com>
2020-12-17 13:49:50 +0200

    added MatchDetails component

---
+++ client/src/components/MatchDetails/MatchDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MatchDetails.scss';
+
+export const MatchDetails = () => {
+    return (
+        <div className='match-details'>
+            <p>Matan</p>
+            <strong>vs</strong>
+            <p>Someone</p>
+        </div>
+    )
+}
