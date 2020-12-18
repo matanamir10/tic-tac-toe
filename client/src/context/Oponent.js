0bf466fddbd059e7ecfcb53a24d6b290159a06d1
matan11 <matan6080@gmail.com>
2020-12-18 18:24:23 +0200

    gameAvailable state

--- client/src/context/Oponent.js
+++ client/src/context/Oponent.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
-import {Player} from '../constants/Player';
+import React, { useState } from "react";
+import { Player } from "../constants/Player";
 
 export const OponentContext = React.createContext();
 
-export const OponentProvider = props =>{
-    const [turn,setTurn] = useState(Player.X);
+export const OponentProvider = (props) => {
+  const [isGameAvailable, setIsGameAvaliable] = useState(false);
+  const [turn, setTurn] = useState(Player.X);
 
-    const setPlayerTurn =()=> setTurn(prevTurn=> prevTurn===Player.X ? Player.O : Player.X); 
+  const setPlayerTurn = () =>
+    setTurn((prevTurn) => (prevTurn === Player.X ? Player.O : Player.X));
 
-    return(
-        <OponentContext.Provider value={{turn,setPlayerTurn}}>
-            {props.children}
-        </OponentContext.Provider>
-    )
-}
+  const setGame = (state) => setIsGameAvaliable(state);
+
+  return (
+    <OponentContext.Provider
+      value={{ turn, setPlayerTurn, isGameAvailable, setGame }}
+    >
+      {props.children}
+    </OponentContext.Provider>
+  );
+};
