1dab2c47dd452c9e76a5424ee70ced259cbde070
matan11 <matan6080@gmail.com>
2020-12-18 13:22:04 +0200

    setup moved from init to listen function

--- server/src/models/serverSocket.ts
+++ server/src/models/serverSocket.ts
@@ -6,20 +6,12 @@ import socketIo from "socket.io";
 // const config = dotenv.config();
 
 export class ServerSocket {
-  constructor() {
-    this.init();
-  }
-
-  private init(): void {
+  listen(): void {
+    console.log("start listenning....");
     //   TODO: use env file
     const app = express();
     app.set("port", 4000);
-
     const server = new http.Server(app);
-  }
-
-  listen(): void {
-    console.log("start listenning....");
     const io = socketIo(http);
     io.on("connection", (socket: SocketIO.Socket) => {
       console.log(socket.id);
