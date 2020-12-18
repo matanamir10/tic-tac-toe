dac8e41ea61310bd8e8f64f1bc8fbc099524df3c
matan11 <matan6080@gmail.com>
2020-12-18 16:12:01 +0200

    added gameId identifyer

--- server/src/models/GameManager.ts
+++ server/src/models/GameManager.ts
@@ -1,14 +1,30 @@
+import { v4 as uuid } from "uuid";
 import { IDisposeable } from "../interfaces/IDisposeable";
 import { Oponent } from "./Oponent";
+import { ServerSocket } from "./serverSocket";
 
 export class GameManager implements IDisposeable {
+  private gameId: string;
+
   constructor(private oponentOne: Oponent, private oponentTwo: Oponent) {
     console.log("in game manager");
-    this.oponentOne.socket.join(this.oponentTwo.socket.id);
-    this.oponentTwo.socket.join(this.oponentOne.socket.id);
+    this.gameId = uuid();
+    this.oponentOne.socket.join([this.gameId, this.oponentTwo.socket.id]);
+    this.oponentTwo.socket.join([this.gameId, this.oponentOne.socket.id]);
+    this.start();
   }
 
-  start(): void {}
+  start(): void {
+    ServerSocket.io.to(this.gameId).emit("game", "Game is stating...");
+    this.oponentOne.socket.on("move", (move) => {});
+
+    this.oponentTwo.socket.on("move", (move) => {});
+    // this.oponentOne.socket
+    //   .to(this.oponentTwo.socket.id)
+    //   .emit("game", {
+
+    //   });
+  }
 
   dispose(): void {
     this.oponentOne.dispose();
