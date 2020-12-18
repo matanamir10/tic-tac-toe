8ce00cd38c0e1b58d8abb2dce356ce78522b3fcc
matan11 <matan6080@gmail.com>
2020-12-18 16:11:22 +0200

    added id and onDisconnet function to notify to queue that user has disconnected

--- server/src/models/Oponent.ts
+++ server/src/models/Oponent.ts
@@ -2,15 +2,21 @@ import { Socket } from "socket.io";
 import { IDisposeable } from "../interfaces/IDisposeable";
 
 export class Oponent implements IDisposeable {
-  constructor(private _socket: Socket) {}
-
-  get socket(): Socket {
-    if (!this._socket.connected) {
-      throw new Error("Socket is not connected");
-    }
-    return this.socket;
+  id: string;
+  constructor(private _socket: Socket) {
+    this.id = "";
   }
 
+  get socket(): Socket {
+    // if (!this._socket.connected) {
+    //   throw new Error("Socket is not connected");
+    // }
+    return this._socket;
+  }
+
+  onDisconnect(func: (id: string) => void): void {
+    this.socket.on("disconnect", func.bind(null, this.id));
+  }
   dispose(): void {
     this.socket.disconnect();
   }
