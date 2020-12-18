9c9d9fe3de648bef40d5ec2ee4645700383880c3
matan11 <matan6080@gmail.com>
2020-12-18 18:24:43 +0200

    setup screen for game

---
+++ client/src/containers/Setup/Setup.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import "./Setup.scss";
+import socket from "../../socket";
+import { OponentContext } from "../../context/Oponent";
+
+export const Setup = () => {
+  const { setGame } = useContext(OponentContext);
+
+  const startConnection = () => {
+    try {
+      socket.connect();
+      const connection = socket.getSocket();
+      console.log(connection);
+      connection.emit("matan", "king");
+      connection.on("connect", () => {
+        console.log("connected");
+        connection.on("game", (data) => {
+          console.log(data);
+          setGame(true);
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log("render");
+  return (
+    <div className="setup">
+      <button onClick={startConnection}>start</button>
+    </div>
+  );
+};
