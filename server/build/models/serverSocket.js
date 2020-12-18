111fcb309f325fa3369c89c88ca2f7b63767cbd4
matan11 <matan6080@gmail.com>
2020-12-18 12:18:34 +0200

    ServerSocket class was decalred

---
+++ server/build/models/serverSocket.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ServerSocket = void 0;
+var ServerSocket = /** @class */ (function () {
+    function ServerSocket() {
+    }
+    return ServerSocket;
+}());
+exports.ServerSocket = ServerSocket;
---
+++ server/src/index.ts
@@ -0,0 +1,4 @@
+import * as dotenv from "dotenv";
+
+const config = dotenv.config();
+console.log(config);
