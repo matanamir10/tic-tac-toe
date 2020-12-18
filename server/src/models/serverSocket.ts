b981d2147e60a89a006e52d0f0fecde535057ab8
matan11 <matan6080@gmail.com>
2020-12-18 16:10:30 +0200

    mako io static to be available for all classes

--- server/src/models/serverSocket.ts
+++ server/src/models/serverSocket.ts
@@ -2,7 +2,7 @@
 import http from "http";
 import express from "express";
 import cors from "cors";
-import socketIO, { Socket } from "socket.io";
+import socketIO, { Socket, Server } from "socket.io";
 import { Queue } from "../models/Queue";
 import { Oponent } from "./Oponent";
 import { GameManager } from "./GameManager";
@@ -10,26 +10,42 @@ import { GameManager } from "./GameManager";
 // const config = dotenv.config();
 
 export class ServerSocket {
+  private static serverSocker: Server;
   private usersManager: Queue<Oponent>;
+
   constructor() {
     this.usersManager = new Queue();
   }
 
+  static get io(): Server {
+    if (!ServerSocket.serverSocker) {
+      throw new Error("Server Socekt is not defined");
+    }
+    return ServerSocket.serverSocker;
+  }
+
   listen(): void {
-    console.log("start listenning....");
-    //   TODO: use env file
     const app = express();
     app.use(cors());
     const server = http.createServer(app);
 
-    const io = socketIO(server);
-    io.on("connection", (socket: Socket) => {
+    ServerSocket.serverSocker = socketIO(server);
+    ServerSocket.serverSocker.on("connection", (socket: Socket) => {
       // If not found usesr waiting no one is available so push him to Queue
       const availeAbleUser = this.usersManager.dequeue();
+      const newOponent = new Oponent(socket);
+      newOponent.onDisconnect((id: string) => {
+        const isRemoved = this.usersManager.remove(id);
+        console.log("is Removed", isRemoved);
+      });
+
       if (!availeAbleUser) {
-        this.usersManager.enqueue(new Oponent(socket));
+        this.usersManager.enqueue(newOponent);
+        console.log("size", this.usersManager.size());
+        return;
       }
-      new GameManager(availeAbleUser, new Oponent(socket));
+
+      new GameManager(availeAbleUser, newOponent);
     });
 
     server.listen(4000);
