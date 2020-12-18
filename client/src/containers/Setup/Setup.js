c62c7837b7d4d1d95f3dd055d5da8f20a0f453f9
matan11 <matan6080@gmail.com>
2020-12-19 00:36:56 +0200

    Added loading when searching for game

--- client/src/containers/Setup/Setup.js
+++ client/src/containers/Setup/Setup.js
@@ -1,20 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Setup.scss";
-import socket from "../../socket";
 import { toast } from "react-toastify";
+import ReactLoading from "react-loading";
+import socket from "../../socket";
 import { OponentContext } from "../../context/Oponent";
 
 export const Setup = () => {
+  const [loading, setLoading] = useState(false);
   const { setGame } = useContext(OponentContext);
 
   const startConnection = () => {
     try {
+      setLoading(true);
       socket.connect();
       const connection = socket.getSocket();
       console.log(connection);
       connection.on("connect", () => {
         console.log("connected");
         connection.on("game", (data) => {
+          setLoading(false);
           console.log(data);
           setGame(true);
         });
@@ -30,6 +34,15 @@ export const Setup = () => {
       <button className="setup__cta" onClick={startConnection}>
         Start Game
       </button>
+      {loading ? (
+        <ReactLoading
+          className="setup__loading"
+          type="spin"
+          color="#c69963"
+          height={50}
+          width={50}
+        />
+      ) : null}
     </div>
   );
 };
