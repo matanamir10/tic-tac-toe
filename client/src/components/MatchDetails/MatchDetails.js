968a93c06f8ed62e4df42aa5bc100eac5235c08d
matan11 <matan6080@gmail.com>
2020-12-19 00:13:33 +0200

    Addtional css

--- client/src/components/MatchDetails/MatchDetails.js
+++ client/src/components/MatchDetails/MatchDetails.js
@@ -1,13 +1,19 @@
 import React, { useContext } from "react";
 import "./MatchDetails.scss";
 import { OponentContext } from "../../context/Oponent";
+import { Player } from "../../constants/Player";
 
 export const MatchDetails = () => {
-  const { turn } = useContext(OponentContext);
+  const { player, turn } = useContext(OponentContext);
 
+  let currentPlayer = turn
+    ? "You are"
+    : player === Player.X
+    ? `Player ${Player.O} is`
+    : `Player ${Player.X} is`;
   return (
     <div className="match-details">
-      <p>Now: {turn.myTurn}</p>
+      <p className="match-details__text">{currentPlayer} playing</p>
     </div>
   );
 };
