5d7074f4fbc7f4ae475cc3f0750b3436c4031680
matan11 <matan6080@gmail.com>
2020-12-18 15:17:16 +0200

    handle avalable users logic

--- server/src/models/serverSocket.ts
+++ server/src/models/serverSocket.ts
@@ -3,10 +3,18 @@ import http from "http";
 import express from "express";
 import cors from "cors";
 import socketIO, { Socket } from "socket.io";
+import { Queue } from "../models/Queue";
+import { Oponent } from "./Oponent";
+import { GameManager } from "./GameManager";
 
 // const config = dotenv.config();
 
 export class ServerSocket {
+  private usersManager: Queue<Oponent>;
+  constructor() {
+    this.usersManager = new Queue();
+  }
+
   listen(): void {
     console.log("start listenning....");
     //   TODO: use env file
@@ -16,7 +24,12 @@ export class ServerSocket {
 
     const io = socketIO(server);
     io.on("connection", (socket: Socket) => {
-      console.log(socket.id);
+      // If not found usesr waiting no one is available so push him to Queue
+      const availeAbleUser = this.usersManager.dequeue();
+      if (!availeAbleUser) {
+        this.usersManager.enqueue(new Oponent(socket));
+      }
+      new GameManager(availeAbleUser, new Oponent(socket));
     });
 
     server.listen(4000);
