588b85afbc333bd38c6653347139c32e4c42cb06
matan11 <matan6080@gmail.com>
2020-12-18 14:44:37 +0200

    added useeffect to crate socket connection

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { ErrorBoundary } from '../../ErrorBoundary/ErrorBoundary';
-import { Game } from '../Game/Game';
-import './App.scss';
+import React, { useEffect } from "react";
+import "./App.scss";
+import { ErrorBoundary } from "../../ErrorBoundary/ErrorBoundary";
+import { Game } from "../Game/Game";
+import socket from "../../socket";
 
 export const App = () => {
+  useEffect(() => {
+    socket.connect();
+    socket.getSocket().on("connect", () => {
+      console.log("connected");
+    });
+  });
   return (
-    <div className='container'>
+    <div className="container">
       <ErrorBoundary>
-      <Game/>
+        <Game />
       </ErrorBoundary>
     </div>
-  )
-}
+  );
+};
