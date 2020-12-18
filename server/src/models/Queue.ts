3196364fe1e2bca30c3a50c1e928c6201db6737d
matan11 <matan6080@gmail.com>
2020-12-18 16:10:44 +0200

    added is to be able to remove item

--- server/src/models/Queue.ts
+++ server/src/models/Queue.ts
@@ -1,7 +1,11 @@
 import { IPrintable } from "../interfaces/IPrintable";
 import { IQueue } from "../interfaces/IQueue";
 
-export class Queue<T> implements IQueue<T>, IPrintable {
+interface QueueItem {
+  id: string;
+}
+
+export class Queue<T extends QueueItem> implements IQueue<T>, IPrintable {
   private storage: T[] = [];
 
   constructor(private capacity: number = Infinity) {}
@@ -11,12 +15,23 @@ export class Queue<T> implements IQueue<T>, IPrintable {
     }
     this.storage.push(item);
   }
+
   dequeue(): T | undefined {
     return this.storage.shift();
   }
+
+  remove(id: string): boolean {
+    const index = this.storage.findIndex((item) => item.id === id);
+    if (index === -1) {
+      return false;
+    }
+    return true;
+  }
+
   size(): number {
     return this.storage.length;
   }
+
   print(): void {
     for (const item of this.storage) {
       console.log(item);
