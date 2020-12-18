37a5dc8d44a42faa7bbcd48b637acc3d2347057a
matan11 <matan6080@gmail.com>
2020-12-18 13:16:49 +0200

    socket server liusten and init app

--- server/src/models/serverSocket.ts
+++ server/src/models/serverSocket.ts
@@ -1,6 +1,28 @@
-import express from 'express';
+import dotenv from "dotenv";
+import http from "http";
+import express from "express";
+import socketIo from "socket.io";
 
+const config = dotenv.config();
 
-export class ServerSocket{
- 
+export class ServerSocket {
+  constructor() {
+    this.init();
+  }
+
+  private init(): void {
+    //   TODO: use env file
+    const app = express();
+    app.set("port", 4000);
+
+    const server = new http.Server(app);
+  }
+
+  listen(): void {
+    console.log("start listenning....");
+    const io = (serverSocket = socketIo(http));
+    io.on("connection", (socket: SocketIO.Socket) => {
+      console.log(socket.id);
+    });
+  }
 }
