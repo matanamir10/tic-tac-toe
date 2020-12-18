7cfec7e2026efd882cdc47f7f34d145a3569acdd
matan11 <matan6080@gmail.com>
2020-12-18 13:21:00 +0200

    remove build files

--- server/build/models/serverSocket.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ServerSocket = void 0;
-var ServerSocket = /** @class */ (function () {
-    function ServerSocket() {
-    }
-    return ServerSocket;
-}());
-exports.ServerSocket = ServerSocket;
