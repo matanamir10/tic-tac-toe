ef93b555e03c17f580d7f4bda5061d6826158282
matan11 <matan6080@gmail.com>
2020-12-19 11:59:41 +0200

    react-modal package installed

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
+import Modal from "react-modal";
 import { App } from "./containers/App/App";
 import { OponentProvider } from "./context/Oponent";
+
+Modal.setAppElement("#root");
 
 ReactDOM.render(
   <React.StrictMode>
